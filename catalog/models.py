966c7bf308e302adcb59dba50e45aa61e86cde0e
Makoto <makoto@mak.ru>
2024-05-20 01:56:37 +0300

    migration 1

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Название")
+    description = models.TextField(max_length=300, verbose_name="Описание")
+    photo = models.ImageField(upload_to="photo", blank=True, null=True)
+    category = models.ForeignKey(
+        "Category", on_delete=models.SET_NULL, null=True, blank=True
+    )
+    price = models.IntegerField(verbose_name="Цена", help_text="Введите цену")
+    created_at = models.DateField(auto_now_add=True, verbose_name="Дата создания")
+    updated_at = models.DateField(
+        auto_now=True, verbose_name="Дата последнего изменения"
+    )
+
+    class Meta:
+        verbose_name = "продукт"
+        verbose_name_plural = "продукты"
+        ordering = ("name", "category")
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Наименованик")
+    description = models.TextField(verbose_name="Описание")
+
+    class Meta:
+        verbose_name = "категория"
+        verbose_name_plural = "категории"
+
+    def __str__(self):
+        return self.name
